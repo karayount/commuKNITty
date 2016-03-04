e1496994ccfee9efd9c4bb076bfc85048312e74c
Kara Yount <kara.m.yount@gmail.com>
2016-03-03 22:46:24 -0800

    two browser tests written and passing

--- client_side_tests.py
+++ client_side_tests.py
@@ -1,51 +1,54 @@
-from selenium import webdriver
 
-browser = webdriver.Chrome()
-browser.get('http://localhost:5000')
-assert browser.title == 'UberCalc'
-
-x = browser.find_element_by_id('x-field')
-x.send_keys("3")
-y = browser.find_element_by_id('y-field')
-y.send_keys("4")
-
-btn = browser.find_element_by_id('calc-button')
-btn.click()
-
-result = browser.find_element_by_id('result')
-assert result.text == "7"
-
-
-
-
-from selenium import webdriver
 import unittest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
 
 class TestCalculator(unittest.TestCase):
 
     def setUp(self):
-        self.browser = webdriver.Chrome()
+        self.browser = webdriver.Firefox()
 
     def tearDown(self):
         self.browser.quit()
 
     def test_title(self):
         self.browser.get('http://localhost:5000/')
-        self.assertEqual(self.browser.title, 'UberCalc')
+        self.assertEqual(self.browser.title, 'Welcome to commuKNITty')
 
-    def test_math(self):
+    def test_landing_page(self):
         self.browser.get('http://localhost:5000/')
+        self.browser.find_element_by_id("nav-profile").click()
+        self.assertIn("You are not authorized", self.browser.page_source)
 
-        x = self.browser.find_element_by_id('x-field')
-        x.send_keys("3")
-        y = self.browser.find_element_by_id('y-field')
-        y.send_keys("4")
+    # def test_math(self):
+    #     self.browser.get('http://localhost:5000/')
+    #
+    #     x = self.browser.find_element_by_id('x-field')
+    #     x.send_keys("3")
+    #     y = self.browser.find_element_by_id('y-field')
+    #     y.send_keys("4")
+    #
+    #     btn = self.browser.find_element_by_id('calc-button')
+    #     btn.click()
+    #
+    #     result = self.browser.find_element_by_id('result')
+    #     self.assertEqual(result.text, "7")
 
-        btn = self.browser.find_element_by_id('calc-button')
-        btn.click()
+    # def test_search_in_python_org(self):
+    #     driver = self.driver
+    #     driver.get("http://www.python.org")
+    #     self.assertIn("Python", driver.title)
+    #     elem = driver.find_element_by_name("q")
+    #     elem.send_keys("pycon")
+    #     elem.send_keys(Keys.RETURN)
+    #     assert "No results found." not in driver.page_source
 
-        result = self.browser.find_element_by_id('result')
-        self.assertEqual(result.text, "7")
+
+    # TESTS TO RUN:
+    # landing page:
+        # pre login, get not authorized flash message
+        # click login
 
 if __name__ == "__main__":
     unittest.main()
